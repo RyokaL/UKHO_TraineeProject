c3b2de7fceee3de645cdb279b1deeaf683e71432
Ryoka <35602890+RyokaL@users.noreply.github.com>
2021-10-29 15:00:07 +0100

    Started making model classes, need to plan DB structure bit more first

---
+++ TraineeProject/Models/PlayerStats.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraineeProject.Models
+{
+    public class PlayerStats
+    {
+        public string CharacterName { get; set; }
+        public int DamagePerSecond { get; set; }
+
+    }
+}
--- TraineeProject/Models/WeatherForecast.cs
+++ TraineeProject/Models/WeatherForecast.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace TraineeProject
+namespace TraineeProject.Models
 {
     public class WeatherForecast
     {
