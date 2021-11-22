80abcdb5264d951dfa928366171da7c3c7dc5988
Ryoka <35602890+RyokaL@users.noreply.github.com>
2021-11-22 15:09:44 +0000

    Began setting up tests

---
+++ TraineeProject/Tests/RepositoryTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FakeItEasy;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using TraineeProject.Database;
+using TraineeProject.Models;
+using TraineeProject.Models.Views;
+using TraineeProject.Repository;
+
+namespace TraineeProject.Tests
+{
+    [TestFixture]
+    public class RepositoryTests
+    {
+        private LogContext _logContext;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var options = new DbContextOptionsBuilder<LogContext>()
+                .UseInMemoryDatabase("logTable").Options;
+            _logContext = new LogContext(options);
+        }
+
+        [Test]
+        public async Task GetPrivateCharacterReturnsNothing()
+        {
+            var repository = A.Fake<ICharacterRepository<CharacterApiView>>();
+
+            var result = await repository.GetAllCharacters();
+
+            Assert.AreEqual(1, result.Count());
+        }
+    }
+}
