2ce8ce54e35b681463abdce9d702dba5a9560e72
Ryoka <35602890+RyokaL@users.noreply.github.com>
2021-12-07 10:38:44 +0000

    Actually add the database files...

---
+++ TraineeProject.Data/Tables/LogParse.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [LogParse] (
+    [Id]            INT             IDENTITY (1, 1) NOT NULL,
+    [InstanceName] NVARCHAR (1000) NOT NULL,
+    [TimeTaken]    INT             NOT NULL,
+    [Succeeded]     BIT             NULL,
+    [DateUploaded] DATETIME        NOT NULL,
+    [Private] BIT NULL, 
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
