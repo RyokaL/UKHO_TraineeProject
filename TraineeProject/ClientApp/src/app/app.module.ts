83e50a3d81181e91221a9c0c8dfc877a2ba9ef52
Ryoka <35602890+RyokaL@users.noreply.github.com>
2022-02-10 17:19:40 +0000

    Add Msal/login framework. Currently broke

--- TraineeProject/ClientApp/src/app/app.module.ts
+++ TraineeProject/ClientApp/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
+import { MsalModule } from '@azure/msal-angular';
+import { PublicClientApplication } from '@azure/msal-browser'
+
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
@@ -11,6 +14,9 @@ import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { CharInfoComponent } from './char-info/char-info.component';
 import { ParseLogComponent } from './parse-log/parse-log.component';
+import { UserLoginOutNavComponent } from './user-login-out-nav/user-login-out-nav.component';
+
+const isIE = window.navigator.userAgent.indexOf('MSIE ') > -1 || window.navigator.userAgent.indexOf('Trident/') > -1;
 
 @NgModule({
   declarations: [
@@ -20,7 +26,8 @@ import { ParseLogComponent } from './parse-log/parse-log.component';
     CounterComponent,
     FetchDataComponent,
     CharInfoComponent,
-    ParseLogComponent
+    ParseLogComponent,
+    UserLoginOutNavComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -32,9 +39,24 @@ import { ParseLogComponent } from './parse-log/parse-log.component';
       { path: 'fetch-data', component: FetchDataComponent },
       { path: 'characters', component: CharInfoComponent },
       { path: 'characters/:id/parses', component: ParseLogComponent },
-    ])
+    ]),
+
+    MsalModule.forRoot( new PublicClientApplication({
+      auth: {
+        clientId: 'b77cbc4e-15f2-475b-8588-0163bcb9499a', // This is your client ID
+        authority: 'b17b2ce2-1c2f-4882-b3a6-469de5140ef2', // This is your tenant ID
+        redirectUri: 'http://localhost:44317'// This is your redirect URI
+      },
+      cache: {
+        cacheLocation: 'localStorage',
+        storeAuthStateInCookie: isIE, // Set to true for Internet Explorer 11
+      }
+    }), null, null),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
---
+++ TraineeProject/ClientApp/src/app/user-login-out-nav/user-login-out-nav.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-login-out-nav',
+  templateUrl: './user-login-out-nav.component.html',
+  styleUrls: ['./user-login-out-nav.component.css']
+})
+export class UserLoginOutNavComponent implements OnInit {
+
+  public isLoggedIn: boolean;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  login() {
+
+  }
+
+  logout() {
+    
+  }
+
+}
