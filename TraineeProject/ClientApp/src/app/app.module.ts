103ea8c82c92ef19f8b671971a2ce5f5f1d582f7
Ryoka <35602890+RyokaL@users.noreply.github.com>
2022-03-02 16:46:51 +0000

    Add some comments to app.module regarding B2C

--- TraineeProject/ClientApp/src/app/app.module.ts
+++ TraineeProject/ClientApp/src/app/app.module.ts
@@ -16,15 +16,19 @@ import { NavMenuLoginOutComponent } from './nav-menu-login-out/nav-menu-login-ou
 import { IPublicClientApplication, PublicClientApplication, InteractionType, BrowserCacheLocation, LogLevel } from '@azure/msal-browser';
 import { MsalGuard, MsalBroadcastService, MsalModule, MsalService, MSAL_GUARD_CONFIG, MSAL_INSTANCE, MsalGuardConfiguration, MsalRedirectComponent } from '@azure/msal-angular';
 
+//Config options for Azure B2C, had to assume based on a few tutorials but seems to work
 export const b2cPolicies = {
   names: {
+      //Name of the user flow for sign up/sign in
       signUpSignIn: "B2C_1_TraineeProjectSignUpSignIn"
   },
+  //These are the endpoints to redirect to for above user flows
   authorities: {
       signUpSignIn: {
           authority: "https://ukhofflogs.b2clogin.com/ukhofflogs.onmicrosoft.com/B2C_1_TraineeProjectSignUpSignIn",
       }
   },
+  //The default authority domain - domain of the B2C tenant
   authorityDomain: "ukhofflogs.b2clogin.com"
 };
 
@@ -32,19 +36,21 @@ export function loggerCallback(logLevel: LogLevel, message: string) {
   console.log(message);
 }
 
+//This is the way Azure tutorial had the config, assuming there's different types of config?
 export function MSALInstanceFactory(): IPublicClientApplication {
   return new PublicClientApplication({
     auth: {
+      //Azure B2C client id, is this a secret? Doesn't say so
       clientId: 'b77cbc4e-15f2-475b-8588-0163bcb9499a',
       authority: b2cPolicies.authorities.signUpSignIn.authority,
       knownAuthorities: [b2cPolicies.authorityDomain],
-      redirectUri: '/',
+      redirectUri: '/', //This must be registered in Azure, it is where the user will be redirected back to
       postLogoutRedirectUri: '/',
       navigateToLoginRequestUrl: true
     },
     cache: {
-      cacheLocation: BrowserCacheLocation.LocalStorage,
-      storeAuthStateInCookie: false, // lol IE11
+      cacheLocation: BrowserCacheLocation.LocalStorage, //Apparently SessionStorage is more secure, but LocalStorage persists the session
+      storeAuthStateInCookie: false, // Seems to be useful for bugs in IE11 but IE11 is unsupported so probably not an issue
     },
     system: {
       loggerOptions: {
@@ -56,6 +62,7 @@ export function MSALInstanceFactory(): IPublicClientApplication {
   });
 }
 
+//MSAL Guard allows routes to only be accessible to logged in users
 export function MSALGuardConfigFactory(): MsalGuardConfiguration {
   return { 
     interactionType: InteractionType.Redirect
@@ -63,6 +70,7 @@ export function MSALGuardConfigFactory(): MsalGuardConfiguration {
 }
 
 @NgModule({
+  //Declares what components are available to Angular
   declarations: [
     AppComponent,
     NavMenuComponent,
@@ -73,6 +81,7 @@ export function MSALGuardConfigFactory(): MsalGuardConfiguration {
     ParseLogComponent,
     NavMenuLoginOutComponent
   ],
+  //Dependency injection section - provides instances to other components that ask for it
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
@@ -86,6 +95,7 @@ export function MSALGuardConfigFactory(): MsalGuardConfiguration {
       { path: 'characters/:id/parses', component: ParseLogComponent },
     ])
   ],
+  //If a component asks for a data structure, provide the function that will give it
   providers: [
     {
       provide: MSAL_INSTANCE,
@@ -99,6 +109,7 @@ export function MSALGuardConfigFactory(): MsalGuardConfiguration {
     MsalGuard,
     MsalBroadcastService
   ],
+  //MsalRedirectComponent needs to be bootstrapped here to handle redirect promises correctly.
   bootstrap: [AppComponent, MsalRedirectComponent]
 })
 export class AppModule { }
