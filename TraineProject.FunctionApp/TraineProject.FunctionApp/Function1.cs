6f94645bbb6ea71d12f7af268cb71c88abc74bc4
Ryoka <35602890+RyokaL@users.noreply.github.com>
2022-05-31 14:36:36 +0100

    Add basic function app

---
+++ TraineProject.FunctionApp/TraineProject.FunctionApp/Function1.cs
@@ -0,0 +1,23 @@
+using System;
+using System.IO;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace TraineProject.FunctionApp
+{
+    public class Function1
+    {
+        [FunctionName("Function1")]
+        public void Run([BlobTrigger("traineeprojectblobstorage/{name}", Connection = "StorageAccountConnectionFunctionApp")]Stream myBlob, string name, ILogger log)
+        {
+            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+            ProcessLog(myBlob);
+        }
+
+        public void ProcessLog(Stream log)
+        {
+            
+        }
+    }
+}
