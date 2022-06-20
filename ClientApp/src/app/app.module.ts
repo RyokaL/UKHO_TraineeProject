63328862769063521f56ebf00fcd4b65db3e4b8e
Ryoka <35602890+RyokaL@users.noreply.github.com>
2022-06-20 15:26:00 +0100

    Remove default components/controllers from template

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -7,8 +7,6 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { CharInfoComponent } from './char-info/char-info.component';
 import { ParseLogComponent } from './parse-log/parse-log.component';
 import { NavMenuLoginOutComponent } from './nav-menu-login-out/nav-menu-login-out.component';
@@ -77,8 +75,6 @@ export function MSALGuardConfigFactory(): MsalGuardConfiguration {
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     CharInfoComponent,
     ParseLogComponent,
     NavMenuLoginOutComponent,
@@ -94,8 +90,6 @@ export function MSALGuardConfigFactory(): MsalGuardConfiguration {
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
       { path: 'characters', component: CharInfoComponent },
       { path: 'characters/:id/parses', component: ParseLogComponent },
       { path: 'profile', component: UserProfileComponent, canActivate: [MsalGuard] }
