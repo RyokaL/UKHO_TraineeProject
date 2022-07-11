2c7e5281a82f42324028e8397a7aa81f62f15afc
Ryoka <35602890+RyokaL@users.noreply.github.com>
2022-07-11 14:11:06 +0100

    Change base URL back

--- ClientApp/src/app/app.api.ts
+++ ClientApp/src/app/app.api.ts
@@ -2,5 +2,5 @@ import { InjectionToken } from "@angular/core";
 
 export const API_URL = new InjectionToken<string>('API_URL', {
     providedIn: 'root',
-    factory: () => 'https://localhost:5001/'
+    factory: () => 'https://calumdbtraineeproject.azurewebsites.net/'
 });
